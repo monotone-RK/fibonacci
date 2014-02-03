e9e0d321865453783f319d0951fcedba9ddad1ef
monotone-RK <kobaya4121@gmail.com>
2014-02-03 15:15:45 +0900

    fixed

--- main.c
+++ main.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int fib(int n) {
-  if(n == 1 || n == 2) return 1;
+  if (n == 1 || n == 2) return 1;
   else return fib(n-1) + fib(n-2);
 }
 
 int main(void) {
   printf("C\n");
       
-  for(int i = 1; i < 20; i++) {
+  for (int i = 1; i < 20; i++) {
     int f = fib(i);
     printf("%d ", f);
   }
